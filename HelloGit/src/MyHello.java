4bff47228ea40a667d6244bd48e1004a6a009cad
KimTaeHwan <KimTaeHwan@DESKTOP-MHQQ28E>
2022-06-20 16:42:39 +0900

    Create V2.0 real

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -3,5 +3,6 @@ public class MyHello {
 	public void sayHello() {
 		System.out.println("Hello!");
 		System.out.println("sayHello change in master branch");
+		System.out.println("REAL v2.0");
 	}
 }
